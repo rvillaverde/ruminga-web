b12ccb21af6ab112c4885d76f35cf7bcc0b3730e
Romina Villaverde <romia.villaverde@gmail.com>
2023-08-11 19:47:11 +0100

    Refactor story api functions

--- src/api/story.ts
+++ src/api/story.ts
@@ -1,22 +1,32 @@
 import { Story } from "@/types/story";
 
-const URL = "https://api.ruminga.com/stories";
+const URL = `${process.env.API_URL}/stories`;
 
 export const fetchStory = async (id: Story["id"]): Promise<Story> =>
-  await fetch(`${URL}/${id}`).then((res) => res.json());
+  await fetch(`${URL}/${id}`)
+    .then((res) => {
+      if (!res.ok) {
+        return Promise.reject();
+      }
+
+      return res.json();
+    })
+    .catch((e) => {
+      console.error(`Error fetching story ${id}`, e);
+    });
 
 export const fetchStories = async (): Promise<Story[]> => {
-  const stories: Story[] = await fetch(URL, {
+  return fetch(URL, {
     next: { tags: ["stories"] },
-  }).then((res) => res.json());
+  })
+    .then((res) => {
+      if (!res.ok) {
+        return Promise.reject();
+      }
 
-  // stories.map(({ es: { name }, photos }) =>
-  //   photos.map(({ background, image }) => {
-  //     console.log("story", name);
-  //     console.log("photo background", background);
-  //     console.log("image", image);
-  //   })
-  // );
-
-  return stories;
+      return res.json();
+    })
+    .catch((e) => {
+      console.error("Error fetching stories", e);
+    });
 };
