9b75732600f3458a367e7b23da2b47bf23007e9c
Romina Villaverde <romia.villaverde@gmail.com>
2023-08-15 23:10:42 +0100

    Update revalidate endpoint

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const tag = request.nextUrl.searchParams.get("tag");
 
   if (!tag) {
